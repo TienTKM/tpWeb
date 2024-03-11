89edd17f2c0afad5eaa669633ce5a3edfecd2478
Tien <tientkm2402@gmail.com>
2024-03-11 16:36:22 +0100

    Premier commit pour les questions 5, 6 et 7

--- interaction.js
+++ interaction.js
@@ -1,22 +1,53 @@
-
 // La création d'un Dnd requière un canvas et un interacteur.
 // L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
 function DnD(canvas, interactor) {
-	// Définir ici les attributs de la 'classe'
+    // Définir ici les attributs de la 'classe'
+    this.xInit = 0;
+    this.yInit = 0;
+    this.xFinal = 0;
+    this.yFinal = 0;
+    this.press = false;
 
-	// Developper les 3 fonctions gérant les événements
+    // Developper les 3 fonctions gérant les événements
+    this.pression = function (evt) {
+        this.press = true;
+        var pos = getMousePosition(canvas, evt);
+        this.xInit = pos.x;
+        this.yInit = pos.y;
+        console.log("Pression : " + this.xInit + " " + this.yInit);
+    }.bind(this);
 
-	// Associer les fonctions précédentes aux évènements du canvas.
-};
+    this.deplacement = function (evt) {
+        if (this.press) {
+            var pos = getMousePosition(canvas, evt);
+            this.xFinal = pos.x;
+            this.yFinal = pos.y;
+            console.log("Déplacement : " + this.xFinal + " " + this.yFinal);
+        }
+    }.bind(this);
+
+    this.relachement = function (evt) {
+        this.press = false;
+        var pos = getMousePosition(canvas, evt);
+        this.xFinal = pos.x;
+        this.yFinal = pos.y;
+        console.log("Relachement : " + this.xFinal + " " + this.yFinal);
+    }.bind(this);
+
+    // Associer les fonctions précédentes aux évènements du canvas.
+    canvas.addEventListener('mousedown', this.pression, false);
+    canvas.addEventListener('mousemove', this.deplacement, false);
+    canvas.addEventListener('mouseup', this.relachement, false);
+}
 
 
 // Place le point de l'événement evt relativement à la position du canvas.
 function getMousePosition(canvas, evt) {
-  var rect = canvas.getBoundingClientRect();
-  return {
-    x: evt.clientX - rect.left,
-    y: evt.clientY - rect.top
-  };
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
+    };
 };
 
 
--- model.js
+++ model.js
@@ -1,3 +1,93 @@
-
 // Implémenter ici les 4 classes du modèle.
+// Drawing
+function Drawing() {
+    this.tabForm = [];
+    this.paint = function (ctx, canvas) {
+        ctx.fillStyle = '#F0F0F0'; // set canvas' background color
+        ctx.fillRect(0, 0, canvas.width, canvas.height);  // now fill the canvas
+        for (const element of this.tabForm) {
+            element.paint(ctx);
+        }
+    }
+    // Implement get form
+    this.getForms = function () {
+        return this.tabForm;
+    }
+}
+
+// Rectangle
+function Rectangle(initx, inity, finalx, finaly, thickness, color) {
+    this.initx = initx;
+    this.inity = inity;
+    this.finalx = finalx;
+    this.finaly = finaly;
+    this.thickness = thickness;
+    this.color = color;
+    this.paint = function (ctx) {
+        ctx.lineWidth = this.thickness;
+        ctx.strokeStyle = this.color;
+        ctx.beginPath();
+        ctx.rect(this.initx, this.inity, this.finalx, this.finaly);
+        ctx.stroke();
+    }
+    // Implement getters
+    this.getInitX = function () {
+        return this.initx;
+    }
+    this.getInitY = function () {
+        return this.inity;
+    }
+    this.getFinalX = function () {
+        return this.finalx;
+    }
+    this.getFinalY = function () {
+        return this.finaly;
+    }
+    // Get thickness and color
+    this.getThickness = function () {
+        return this.thickness;
+    }
+    this.getColor = function () {
+        return this.color;
+    }
+}
+
+// Line
+function Line(initx, inity, finalx, finaly, thickness, color) {
+    this.initx = initx;
+    this.inity = inity;
+    this.finalx = finalx;
+    this.finaly = finaly;
+    this.thickness = thickness;
+    this.color = color;
+    this.paint = function (ctx) {
+        ctx.lineWidth = this.thickness;
+        ctx.strokeStyle = this.color;
+        ctx.beginPath();
+        ctx.moveTo(this.initx, this.inity);
+        ctx.lineTo(this.finalx, this.finaly);
+        ctx.stroke();
+    }
+    // Implement getters
+    this.getInitX = function () {
+        return this.initx;
+    }
+    this.getInitY = function () {
+        return this.inity;
+    }
+    this.getFinalX = function () {
+        return this.finalx;
+    }
+    this.getFinalY = function () {
+        return this.finaly;
+    }
+    // Get thickness and color
+    this.getThickness = function () {
+        return this.thickness;
+    }
+    this.getColor = function () {
+        return this.color;
+    }
+}
+
 // N'oubliez pas l'héritage !
