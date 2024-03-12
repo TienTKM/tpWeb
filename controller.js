d63d3fdd38414029ff1009e2ee26470d62e2e512
Pruvost Raphael <raphael.pruvost@etudiant.univ-rennes1.fr>
2024-03-12 15:27:02 +0100

    Q9-10

--- controller.js
+++ controller.js
@@ -5,13 +5,50 @@ function Pencil(ctx, drawing, canvas) {
 	this.currEditingMode = editingMode.line;
 	this.currLineWidth = 5;
 	this.currColour = '#000000';
-	this.currentShape = 0;
+	this.currentShape = null;
 
 	// Liez ici les widgets à la classe pour modifier les attributs présents ci-dessus.
 
-	new DnD(canvas, this);
+    new DnD(canvas, this);
 
-	// Implémentez ici les 3 fonctions onInteractionStart, onInteractionUpdate et onInteractionEnd
+    this.onInteractionStart = function(dnd) {
+        var x = dnd.xInit;
+        var y = dnd.yInit;
+        
+        if (this.currEditingMode === editingMode.rect) {
+            this.currentShape = new Rectangle(x, y, x, y, this.currLineWidth, this.currColour);
+        } else if (this.currEditingMode === editingMode.line) {
+            this.currentShape = new Line(x, y, x, y, this.currLineWidth, this.currColour);
+        }
+
+        if (this.currentShape) {
+            drawing.tabForm.push(this.currentShape);
+        }
+        
+        drawing.paint(ctx, canvas);
+    };
+
+    this.onInteractionUpdate = function(dnd) {
+        var x = dnd.xFinal;
+        var y = dnd.yFinal;
+
+        if (this.currentShape) {
+            this.currentShape.finalx = x;
+            this.currentShape.finaly = y;
+            drawing.paint(ctx, canvas);
+        }
+    };
+
+    this.onInteractionEnd = function(dnd) {
+    if (this.currentShape) {
+        var x = dnd.xFinal;
+        var y = dnd.yFinal;
+        this.currentShape.finalx = x;
+        this.currentShape.finaly = y;
+        this.currentShape = null;
+        drawing.paint(ctx, canvas);
+    }
+    };
 };
 
 
